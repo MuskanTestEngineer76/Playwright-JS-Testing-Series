2e047fba709ac72e32a1b63ec923c97fbbdd81aa
MuskanTestEngineer76 <muskanambardekar8@gmail.com>
2026-04-30 19:56:05 +0530

    Day 3 : Effortless code generation using codegen

---
+++ tests/OrangeHRM_CodeGen.spec.js
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+test('google Website code gen', async ({ page }) => {
+  await page.goto('https://www.google.com/');
+  await page.getByRole('button', { name: 'I\'m Feeling Lucky' }).click();
+ 
+});
