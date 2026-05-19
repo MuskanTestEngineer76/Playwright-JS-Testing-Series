17d8f23996803c69921d77649cffb8e67b691b25
MuskanTestEngineer76 <muskanambardekar8@gmail.com>
2026-05-19 19:06:39 +0530

    add image folder

--- tests/Uploading.spec.js
+++ tests/Uploading.spec.js
@@ -1,49 +1,79 @@
-import {test, expect} from "@playwright/test";
-import { buffer } from "node:stream/consumers";
+import { test, expect } from "@playwright/test";
+import path from "path";
+
+//file paths
+const jkRowlingFile = path.join(__dirname, "../uploads/J. K. Rowling.jpg");
+const johnGreenFile = path.join(__dirname, "../uploads/John Green.jpg");
 
 //uploading single file
-test.skip("Upload single files", async ({page}) =>{
+test.skip("Upload single files", async ({ page }) => {
+
     await page.goto("https://west-wind.com/wconnect/wcscripts/fileupload.wwd");
-    await page.locator("//input[@id='upload']").setInputFiles("C:/Users/muska/Downloads/J. K. Rowling.jpg");
+
+    await page.locator("#upload").setInputFiles(jkRowlingFile);
+
     await page.waitForTimeout(5000);
-})
+});
 
 //uploading multiple files
-test.skip("upload multiple files", async ({page}) =>{
+test.skip("upload multiple files", async ({ page }) => {
+
     await page.goto("https://west-wind.com/wconnect/wcscripts/fileupload.wwd");
-    await page.locator("//input[@id='upload']").setInputFiles(["C:/Users/muska/Downloads/J. K. Rowling.jpg", "C:/Users/muska/Downloads/John Green.jpg"]);
+
+    await page.locator("#upload")
+        .setInputFiles([jkRowlingFile, johnGreenFile]);
+
     await page.waitForTimeout(5000);
-})
+});
 
 //uploading buffer file
-test.skip("upload buffer file", async ({page}) =>{
-    await page.goto("https://west-wind.com/wconnect/wcscripts/fileupload.wwd")
-    await page.locator("//input[@id='upload']").setInputFiles({
+test.skip("upload buffer file", async ({ page }) => {
 
-        name: 'playwright.txt',
-        mimeType: 'text/plain',
-        buffer: Buffer.from('This is a text')
-    })
+    await page.goto("https://west-wind.com/wconnect/wcscripts/fileupload.wwd");
+
+    await page.locator("#upload").setInputFiles({
+
+        name: "playwright.txt",
+        mimeType: "text/plain",
+        buffer: Buffer.from("This is a text")
+    });
+
     await page.waitForTimeout(5000);
-})
+});
 
 //uploading file without input type
-test.skip("upload file without input type", async ({page}) =>{
+test.skip("upload file without input type", async ({ page }) => {
+
     await page.goto("https://easyupload.io/");
-    const fileChooserPromise = page.waitForEvent('filechooser');
+
+    const fileChooserPromise = page.waitForEvent("filechooser");
+
     await page.locator("//span[@type='button']").click();
+
     const fileChooser = await fileChooserPromise;
-    await fileChooser.setFiles('C:/Users/muska/Downloads/J. K. Rowling.jpg');
+
+    await fileChooser.setFiles(jkRowlingFile);
 
     await page.waitForTimeout(5000);
-
-})
+});
 
 //removing uploaded file
-test("remove uplaoded file", async ({page}) =>{
-    await page.goto("https://west-wind.com/wconnect/wcscripts/fileupload.wwd");
-    await page.locator("//input[@id='upload']").setInputFiles(["C:/Users/muska/Downloads/J. K. Rowling.jpg", "C:/Users/muska/Downloads/John Green.jpg"]);
+test("remove uploaded file", async ({ page }) => {
+
+    await page.goto(
+        "https://west-wind.com/wconnect/wcscripts/fileupload.wwd",
+        {
+            waitUntil: "domcontentloaded",
+            timeout: 60000
+        }
+    );
+
+    await page.locator("#upload")
+        .setInputFiles([jkRowlingFile, johnGreenFile]);
+
     await page.waitForTimeout(5000);
-    await page.locator("//input[@id='upload']").setInputFiles([]);
+
+    await page.locator("#upload").setInputFiles([]);
+
     await page.waitForTimeout(5000);
-})
+});
