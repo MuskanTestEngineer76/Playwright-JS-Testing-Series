f9036b09eeb222367193c7846352de68c70a4e4a
MuskanTestEngineer76 <muskanambardekar8@gmail.com>
2026-05-22 19:34:26 +0530

    code updated

--- tests/OrangeHRM_Locators.spec.js
+++ tests/OrangeHRM_Locators.spec.js
@@ -1,15 +1,20 @@
-import{test, expect} from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
-test("locate orange hrm website element using different locators",  async ({page})=>{
+test("locate orange hrm website element using different locators", async ({ page }) => {
+
     await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
 
-    await page.getByLabel('Password');                         //locate by lable
-    await page.getByPlaceholder("Username").fill("Admin")      //locate by pleaceholder
-    await page.getByPlaceholder("Password").fill("admin123");    //locate by placeholder
-    await page.getByRole('button', {name: 'Login'}).click();    //locate by role
-    await page.getByAltText('profile picture').click();         //locate by alt text
-    await page.getByText("Logout").click();                     //locate by text
-    await page.waitForTimeout(5000);                            //add wait
+    await page.getByPlaceholder("Username").fill("Admin");
 
+    await page.getByPlaceholder("Password").fill("admin123");
 
-})
+    await page.getByRole('button', { name: 'Login' }).click();
+
+    // More specific locator
+    await page.locator('.oxd-userdropdown-img').click();
+
+    await page.getByText("Logout").click();
+
+    await page.waitForTimeout(5000);
+
+});
